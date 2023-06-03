e3b6a3f770399edeeeec4113646ac1074720546b
sebastian-sobik <sebasob749@student.polsl.pl>
2023-06-03 19:23:25 +0200

    Small project fixes

---
+++ jamf-assignment/src/app/shared/initFakeProducts.ts
@@ -0,0 +1,28 @@
+import {Product} from "./product.model";
+
+export const initProducts : Product[] = [
+  {
+    id: 0,
+    name: "MacBook Air 13”",
+    price: 4999,
+    imageURL: "https://images.macrumors.com/t/EM2og9plsP9SVnf3gfrHFAVStNU=/800x0/article-new/2022/06/2016-12-inch-macbook-feature.jpg?lossy"
+  },
+  {
+    id: 1,
+    name: "iMac 27’’",
+    price: 7499,
+    imageURL: "https://erecruiter.pl/wp-content/uploads/2021/02/marketing-rekrutacyjny.jpg"
+  },
+  {
+    id: 2,
+    name: 'iPad air M1',
+    price: 2999.90,
+    imageURL: "https://images.immediate.co.uk/production/volatile/sites/3/2022/03/iPad-Air-5-main--190e689.jpg?quality=90&webp=true&resize=620,414"
+  },
+  {
+    id: 3,
+    name: 'Apple watch 7',
+    price: 2649,
+    imageURL: "https://iphoneislam.com/wp-content/uploads/2022/05/158682-smartwatches-review-apple-watch-series-7-all-about-the-screen-image2-ggnlijys37-675x450.jpg"
+  }
+]
