caaaab705ff97e08f8fbab031d5059be5aa1b0ee
sebastian-sobik <sebasob749@student.polsl.pl>
2023-06-05 17:49:58 +0200

    On-scroll box-shadow

--- jamf-assignment/src/app/header/header.component.ts
+++ jamf-assignment/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, HostListener, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,12 @@ import {Component, EventEmitter, Output} from '@angular/core';
 })
 export class HeaderComponent {
   @Output() openSidebar = new EventEmitter<void>;
+
+  scrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.scrolled = window.scrollY > 10;
+  }
 }
-``
+
